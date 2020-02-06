672a6b3b621d89fef15112057a97a70de9a3f622
Kuddleman <edl1155@gmail.com>
2020-02-05 18:33:58 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+
+const random = Math.ceil(Math.random())
+
+
+const mod = 24 % 5
+
+
+const max = Math.max(1,3,6,7,8,10,20)
